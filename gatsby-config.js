815ab769c29d9226f92ef2a1d8b3273ce65d71d1
Gordon <glewis@berkeley.edu>
2021-06-04 20:49:12 -0700

    Modify netlify plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`, // for custom preview in the Netlify CMS
+        customizeWebpackConfig: (config) => (config.node.fs = 'empty'),
       },
     },
   ],
